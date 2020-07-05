aef69e1daf95388b5ca22b183e62e81f9f6aa48b
PRATIK DHARAMDASANI <pratik@commutatus.com>
2020-07-05 20:08:59 +0530

    remove credentials

--- app/controllers/live_sessions_controller.rb
+++ app/controllers/live_sessions_controller.rb
@@ -6,12 +6,12 @@ class LiveSessionsController < ApplicationController
 
     def get_signature
         time = (Time.now.to_i * 1000 - 30000).to_s
-        data = Base64.urlsafe_encode64(Rails.application.credentials.zoom[:zoom_api_key] + params[:meeting_number] + time + params[:role].to_s)
-        hash = Base64.urlsafe_encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), Rails.application.credentials.zoom[:zoom_api_secret], data))
-        tempStr = Rails.application.credentials.zoom[:zoom_api_key] + '.' + params[:meeting_number] + '.' + time + '.' + params[:role].to_s + '.' + hash
+        data = Base64.urlsafe_encode64(<zoom_api_key> + params[:meeting_number] + time + params[:role].to_s)
+        hash = Base64.urlsafe_encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), <zoom_api_secret>, data))
+        tempStr = <zoom_api_key> + '.' + params[:meeting_number] + '.' + time + '.' + params[:role].to_s + '.' + hash
         signature = Base64.urlsafe_encode64(tempStr).gsub('+', '-').gsub('/', '_').gsub(/#{Regexp.escape('=')}+$/, '')
 
-        render json: {signature: signature, meetingNumber: params[:meeting_number], apiKey: Rails.application.credentials.zoom[:zoom_api_key], userName: params[:username], userEmail: params[:email], password: params[:password] }
+        render json: {signature: signature, meetingNumber: params[:meeting_number], apiKey: <zoom_api_key>, userName: params[:username], userEmail: params[:email], password: params[:password] }
     end
     
 end
